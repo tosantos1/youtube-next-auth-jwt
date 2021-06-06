ba80f13ab4141f2411aa78302748850f6e8501f4
Tiago Oliveira <tiagolivesan@outlook.com>
2021-06-06 12:37:09 -0300

    next-auth-jwt

---
+++ src/services/auth.ts
@@ -0,0 +1,21 @@
+import { v4 as uuid } from 'uuid'
+
+type SignInRequestData = {
+    email: string;
+    password: string;
+}
+
+const delay = (amount = 750) => new Promise(resolve => setTimeout(resolve, amount))
+
+export async function signInRequest(data: SignInRequestData) {
+    await delay()
+
+    return {
+        token: uuid(),
+        user: {
+            name: 'Tiago Oliveira',
+            email: 'tiagolivesan@outlook.com',
+            avatar_url: 'https://github.com/tosantos1.png'
+        }
+    }
+}
